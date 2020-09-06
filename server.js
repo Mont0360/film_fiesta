de86f8f09e7afc579238a706fa32c0394f6bd061
Pangsua Vang <pangsua26@yahoo.com>
2020-09-06 16:32:11 -0500

    added code to server js and updated package versions

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,7 +19,19 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
